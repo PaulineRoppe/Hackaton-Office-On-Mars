be41e9254f2454667f4e637b555151fdb47b00b3
bovealexandre <alexandre.l.bove@gmail.com>
2019-04-27 22:54:05 +0200

    merging datas with front

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, { PropTypes,useState, setState } from 'react';
+import React, { PropTypes,useState, setState, useEffect } from 'react';
 import './App.css';
 import { Planet } from './Planet';
 import { InfoPlanet } from './InfoPlanet';
 import { InfoMeteo } from './InfoMeteo';
-// import useAxios from 'axios-hooks';
 import axios from 'axios'
 import Speech from 'speak-tts'
 
-const App =  async (props) => {
-  let response
-  
-  response = await fetch('api/datas')
-  // const [ response, loading, error , refetch] = useAxios({
-  //   method: 'GET',
-  //   url:'api/datas'
-  // })
-  // 'latitude' : data.latitude, 'longitude' : data.longitude
-  console.log(response.data.longitude)
+export const App =  (props) => {
   const [tempMedium, setTempMedium] = useState('25 °C');
-  const [coord, setCoord] = useState({});
+  const [coord, setCoord] = useState({'latitude' : '', 'longitude' : ''});
   const [day, setDay] = useState('Vendredi 25 Avril 2025');
   const [time, setTime] = useState('15h15');
   const [atmos, setAtmos] = useState();
   const [danger, setDanger] = useState(false);
   const [params, setParams] = useState();
   const [tabData, setData] = useState([{ 'time': '16H00', 'wind': '750 hpa', 'temp': '25°', 'rad': '32 msv' }])
+
+
+
+  fetch('api/datas').then(function(response) {
+    return response.json();
+  }).then(function (json) {
+    console.log(json.longitude);
+    setCoord({'latitude' : json.latitude, 'longitude' : json.longitude})
+  })
+
+
+  // const [ response, loading, error , refetch] = useAxios({
+  //   method: 'GET',
+  //   url:'api/datas'
+  // })
+  // 'latitude' : data.latitude, 'longitude' : data.longitude
+
   
   
   
@@ -37,6 +44,3 @@ const App =  async (props) => {
     </div>
   );
 }
-
-
-export default App;
