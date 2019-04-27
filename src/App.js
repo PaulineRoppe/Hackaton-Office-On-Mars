537318b95be0cb620c0efd3510496fd912c4d7d5
bovealexandre <alexandre.l.bove@gmail.com>
2019-04-27 08:14:24 +0200

    Integration of Voice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, {useState, setState} from 'react';
-import logo from './logo.svg';
+import React, { useState, setState} from 'react';
 import './App.css';
 import {Planet} from './Planet';
 import {InfoPlanet} from './InfoPlanet';
 import {InfoMeteo} from './InfoMeteo';
 
-const App = () => {
+
+
+const App = (props) => {
   const [tempMedium , setTempMedium] = useState('25 °C');
   const [coord, setCoord] = useState(['Latitude : 50.6412', 'Longitude : 5.5718']);
   const [day, setDay] = useState('Vendredi 25 Avril 2025');
@@ -15,14 +16,16 @@ const App = () => {
   const [params, setParams] = useState();
   const [tabData, setData] = useState([{'time' : '16H00', 'wind' : '750 hpa', 'temp' : '25°', 'rad' : '32 msv' }])
 
+  setTimeout(window.responsiveVoice.speak('Steph féminin a la mega tete dans le cul', 'French Female'), 500)
+  
   return (
-    <div className="App">
-      <Planet coord={coord} />
-      <InfoPlanet day={day} time={time} temp={tempMedium} coord={coord} />
-      <InfoMeteo tabData={tabData} />
-
-    </div>
+      <div className="App">
+        <Planet coord={coord} />
+        <InfoPlanet day={day} time={time} temp={tempMedium} coord={coord} />
+        <InfoMeteo tabData={tabData} />
+      </div>
   );
 }
+
 
 export default App;
