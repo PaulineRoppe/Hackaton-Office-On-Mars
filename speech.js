ff17df8f98e84f7df152119219fa9ba290c204a0
Upd4ting <upd4ting@gmail.com>
2019-04-28 03:30:45 +0200

    speech to text

---
+++ speech.js
@@ -0,0 +1,28 @@
+
+exports.speechToTextGoogle = function (arrayBuffer, endpoint, key, languageCode, callback) {
+    const url = endpoint + '?key=' + key;
+    const audio = arrayBufferToBase64(arrayBuffer);
+    const body = {
+      config : {
+        audioChannelCount: 2,
+        languageCode
+      },
+      audio : {
+        content: audio
+      }
+      };
+    
+    let request = require('request');
+
+    request.post(url, body, callback);
+};
+ 
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array( buffer );
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
