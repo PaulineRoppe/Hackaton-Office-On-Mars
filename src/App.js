664cb636b56a3145723c6c1352851f17a6770524
bovealexandre <alexandre.l.bove@gmail.com>
2019-04-28 03:10:52 +0200

    fix some bugs & finish contacting the database

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import { Planet } from './Planet';
 import { InfoPlanet } from './InfoPlanet';
 import { InfoMeteo } from './InfoMeteo';
+import axios from 'axios'
 import Speech from 'speak-tts'
 
 export const App =  (props) => {
-  const [tempMedium, setTempMedium] = useState('25 °C');
-  const [coord, setCoord] = useState({'latitude' : '', 'longitude' : ''});
-  const [day, setDay] = useState('Vendredi 25 Avril 2025');
-  const [time, setTime] = useState('15h15');
+  const [tempMedium, setTempMedium] = useState();
+  const [coord, setCoord] = useState({});
+  const [day, setDay] = useState();
+  const [time, setTime] = useState();
   const [atmos, setAtmos] = useState();
   const [danger, setDanger] = useState(false);
   const [params, setParams] = useState();
-  const [tabData, setData] = useState([{ 'time': '16H00', 'wind': '750 hpa', 'temp': '25°', 'rad': '32 msv' }])
+  const [tabData, setData] = useState()
+  const [temps, setTemps] = useState([])
 
 
   console.log('test')
@@ -27,14 +29,22 @@ export const App =  (props) => {
   useEffect(() => {
     axios
       .get('api/datas')
-      .then(result => setData(result.data));
+      .then(result => {  
+        console.log(result.data)
+        setTempMedium(result.data.temp)
+        setCoord({'longitude' : result.data.longitude , 'latitude' : result.data.latitude})
+        setData({ 'atmosphere' : result.data.atmosphere, 'rad' : result.data.radiation})
+        setTemps(result.data.temperatures)
+        setTempMedium(result.data.temperature)
+        setDanger(result.data.alert)
+    });
   }, []);
 
   return (
     <div className="App">
       <Planet coord={coord} />
       <InfoPlanet day={day} time={time} temp={tempMedium} coord={coord} />
-      <InfoMeteo tabData={tabData} />
+      <InfoMeteo temps ={temps} tabData={tabData} />
     </div>
   );
 }
